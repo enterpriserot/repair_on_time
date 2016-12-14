f66eb4dfbef7bf4cdc03da60e86eac19ea561ecb
Antonio Revert <a.revert.hinojosa@gmail.com>
2016-12-14 16:21:44 +0100

    Added several files to server_routes.sh to change in the server deploy

--- server_routes.sh
+++ server_routes.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-replace "http://localhost/repairontime" "https://" -- ./modules/users/view/js/init.js
+replace "http://localhost/repair_on_time/" "https://" -- ./modules/users/view/js/init.js
+replace "http://localhost/repair_on_time/" "https://" -- ./modules/users/view/modal.html
+replace "http://" "https://" -- ./paths.php
+replace "/repair_on_time/" "/" -- ./paths.php
+replace "/repair_on_time/modules/technicians/view/img/" "/modules/technicians/view/img/" -- ./modules/technicians/view/js/geolocator.js
+replace "http://localhost/repair_on_time" "https://repairontime.tk" -- ./view/js/main.js
