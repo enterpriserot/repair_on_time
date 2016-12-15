7bdf05be97aa4413325dff997f45409e02c52029
Antonio Revert <a.revert.hinojosa@gmail.com>
2016-12-15 10:13:00 +0100

    Added files to server_routes.sh and others chages to do more easy deploy in the server

--- server_routes.sh
+++ server_routes.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-replace "http://localhost/repair_on_time/" "https://" -- ./modules/users/view/js/init.js
-replace "http://localhost/repair_on_time/" "https://" -- ./modules/users/view/modal.html
+replace "/repair_on_time/" "/" -- .htaccess
+replace "http://localhost/repair_on_time/" "//repairontime.tk/" -- ./modules/users/view/js/init.js
+replace "http://localhost/repair_on_time/" "//repairontime.tk/" -- ./modules/users/view/modal.html
 replace "http://" "https://" -- ./paths.php
 replace "/repair_on_time/" "/" -- ./paths.php
 replace "/repair_on_time/modules/technicians/view/img/" "/modules/technicians/view/img/" -- ./modules/technicians/view/js/geolocator.js
-replace "http://localhost/repair_on_time" "https://repairontime.tk" -- ./view/js/main.js
+replace "http://localhost/repair_on_time" "//repairontime.tk" -- ./view/js/main.js
+mkdir -p log/general
+touch ./log/general/Site_General_errors.log
+chmod 777 ./log/general/Site_General_errors.log
+chown -R $USER:www-data *
+chmod 755 -R *
+echo "Faltan los siguientes archivos:"
+echo "./model/db.ini"
+mv ./model/dbini ./model/db.ini
+echo "./classes/email/email.ini"
+mv ./classes/email/emailini ./classes/email/email.ini
+echo "./libs/PHPMailer_v5.1"
+mv /libs/PHPMailer_v5.1/docsini ./classes/PHPMailer_v5.1/docs.ini
